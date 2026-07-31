9a771a841a701a741bf1d6c72ca7cf270c08fcc2
preetramanuj <preetramanuj1001@gmail.com>
2026-07-31 10:03:41 +0530

    first commit

---
+++ Dictionary2.py
@@ -0,0 +1,6 @@
+dic={}
+for i in range(5):
+    k=input("Enter key:")
+    v=input("Enter value:")
+    dic[k]=v
+print(dic)
---
+++ for loop5.py
@@ -0,0 +1,6 @@
+a=int(input("Enter num1:"))
+b=int(input("Enter num2:"))
+c=0
+for a in range(a+1,b):
+    c+=1
+print(c)
