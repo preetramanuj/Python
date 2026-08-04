4717924defb1e5e6d9a7e72817f6701fb7be2fd5
Preet Ramanuj <preetramanuj1001@gmail.com>
2026-08-04 16:49:02 +0530

    New OOP Program for Python

---
+++ OOP_Python4.py
@@ -0,0 +1,7 @@
+#without parameter function with return type
+def student_num():
+    a=[]
+    for i in range(5):
+        num=int(input("Enter the number of student: "))
+        a.append(num)
+    return a
