3ed9c6fe880ffcae0cbf97c89ea910fc69ad7210
Preet Ramanuj <preetramanuj1001@gmail.com>
2026-08-11 15:59:09 +0530

    Delete OOP_Python5.py

--- OOP_Python5.py
+++
@@ -1,9 +0,0 @@
-
-#with parameter function with return type
-def student_num(a):
-    a=[]
-    for i in range(5):
-        num=int(input("Enter the number of student: "))
-        a.append(num)
-
-    return a
