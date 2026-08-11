aa34d62f7444a93ec0230062e3d3351720b242e6
Preet Ramanuj <preetramanuj1001@gmail.com>
2026-08-11 16:06:14 +0530

    Delete pr2.py

--- pr2.py
+++
@@ -1,8 +0,0 @@
-Name=input("Enter Name:")
-a=int(input(F"Enter  age of {Name}: "))
-if(a>=18):
-    print(Name,"are eligible for vote")
-if(a<18 and a>0):
-    print(Name,"are eligible not vote,",Name,"need",18-a,"for voting")
-if(a<=0):
-    print(Name,"invalid age")
